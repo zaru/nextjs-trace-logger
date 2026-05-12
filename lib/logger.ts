342561b2680134cd18c90fde6b8317194dec1949
zaru <zarutofu@gmail.com>
2026-05-12 13:52:08 +0900

    set to pretty log if dev env

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,4 +2,14 @@ import pino from "pino";
 
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? "info",
+  transport: process.stdout.isTTY
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:HH:MM:ss",
+          ignore: "pid,hostname",
+        },
+      }
+    : undefined,
 });
